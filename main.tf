4d1c16bec4edc5f7dfd8caeb29bd9a36595fa7bb
balu2422 <157208069+balu2422@users.noreply.github.com>
2025-04-04 10:22:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,16 +2,16 @@ provider "aws" {
   region = "us-west-2"
 }
 
-resource "aws_s3_bucket" "mybuckt1122" {
-  bucket = "mybuckt1122"
+resource "aws_s3_bucket" "mybuckt1122hcl1" {
+  bucket = "mybuckt1122hcl1"
 
   versioning {
     enabled = true
   }
 }
 
-resource "aws_s3_bucket_server_side_encryption_configuration" "mybuckt1122" {
-  bucket = aws_s3_bucket.mybuckt1122.bucket
+resource "aws_s3_bucket_server_side_encryption_configuration" "mybuckt1122hcl1" {
+  bucket = aws_s3_bucket.mybuckt1122hcl1.bucket
 
   rule {
     apply_server_side_encryption_by_default {
@@ -27,7 +27,7 @@ resource "null_resource" "create_index_file" {
 }
 
 resource "aws_s3_object" "index_file" {
-  bucket = aws_s3_bucket.mybuckt1122.bucket
+  bucket = aws_s3_bucket.mybuckt1122hcl1.bucket
   key    = "index.html"
   source = "index.html"
   acl    = "public-read"
