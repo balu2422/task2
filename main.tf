ffbabcecfc3bb9b4f37707462dc687d6a58880e4
balu2422 <157208069+balu2422@users.noreply.github.com>
2025-04-04 10:27:22 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,16 +2,16 @@ provider "aws" {
   region = "us-west-2"
 }
 
-resource "aws_s3_bucket" "mybuckt1122hcl1" {
-  bucket = "mybuckt1122hcl1"
+resource "aws_s3_bucket" "mytask_2bucket_hcl" {
+  bucket = "mytask-2bucket-hcl"
 
   versioning {
     enabled = true
   }
 }
 
-resource "aws_s3_bucket_server_side_encryption_configuration" "mybuckt1122hcl1" {
-  bucket = aws_s3_bucket.mybuckt1122hcl1.bucket
+resource "aws_s3_bucket_server_side_encryption_configuration" "mytask_2bucket_hcl" {
+  bucket = aws_s3_bucket.mytask_2bucket_hcl.bucket
 
   rule {
     apply_server_side_encryption_by_default {
@@ -27,10 +27,9 @@ resource "null_resource" "create_index_file" {
 }
 
 resource "aws_s3_object" "index_file" {
-  bucket = aws_s3_bucket.mybuckt1122hcl1.bucket
+  bucket = aws_s3_bucket.mytask_2bucket_hcl.bucket
   key    = "index.html"
   source = "index.html"
-  acl    = "public-read"
 
   depends_on = [null_resource.create_index_file]
 }
